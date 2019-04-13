e53fd5a35ad44b87c4786ee93e5abb8be75c9f00
snolan98k <snolan98k@gmail.com>
2019-04-13 00:23:36 -0400

    game

---
+++ assets/javascript/game.js
@@ -0,0 +1,110 @@
+$(document).ready(function () {
+
+
+
+    var compPick = Math.floor(Math.random() * 101) + 19;
+    console.log("compPick: " + compPick);
+    $(".randomNumber").html(compPick);
+
+
+
+    var gemOne = Math.floor(Math.random() * 12) + 1;
+    console.log("Gem 1: " + gemOne);
+    $("#img1").html("<img src=" + "assets/images/blue.jpg" + "value" + gemOne + ">")
+        ;
+
+    var gemTwo = Math.floor(Math.random() * 12) + 1;
+    console.log("Gem 2: " + gemTwo);
+    $("#img2").html("<img src=" + "assets/images/pink.jpg" + " value=" + gemTwo + ">");
+
+    var gemThree = Math.floor(Math.random() * 12) + 1;
+    console.log("Gem 3: " + gemThree);
+    $("#img3").html("<img src=" + "assets/images/white.jpg" + " value=" + gemThree + ">");
+
+    var gemFour = Math.floor(Math.random() * 12) + 1;
+    console.log("Gem 4: " + gemFour);
+    $("#img4").html("<img src=" + "assets/images/green.jpg" + " value=" + gemFour + ">");
+
+    var wins = 0;
+    console.log("wins: " + wins);
+
+    var losses = 0;
+    console.log("losses: " + losses);
+
+    var score = 0;
+    console.log("score: " + score);
+
+    function reset() {
+        compPick = Math.floor(Math.random() * 101) + 19;
+        console.log("compPick: " + compPick);
+        $(".randomNumber").html(compPick);
+
+        score = 0;
+        $(".scoreDisplay").html(score);
+
+        gemOne = Math.floor(Math.random() * 12) + 1;
+        console.log("Gem 1: " + gemOne);
+        $("#img1").html("<img src=" + "assets/images/blu.jpg" + "value" + gemOne + ">");
+
+        gemTwo = Math.floor(Math.random() * 12) + 1;
+        console.log("Gem 2: " + gemTwo);
+        $("#img2").html("<img src=" + "assets/images/pink.jpg" + " value=" + gemTwo + ">");
+
+        gemThree = Math.floor(Math.random() * 12) + 1;
+        console.log("Gem 3: " + gemThree);
+        $("#img3").html("<img src=" + "assets/images/white.jpg" + " value=" + gemThree + ">");
+
+        gemFour = Math.floor(Math.random() * 12) + 1;
+        console.log("Gem 4: " + gemFour);
+        $("#img4").html("<img src=" + "assets/images/green.jpg" + " value=" + gemFour + ">");
+
+
+        $("img").on("click", function () {
+            var newScore = score += parseInt($(this).attr("value"));
+            console.log("New Score: " + newScore);
+            $(".scoreDisplay").html(newScore);
+
+            if (newScore === compPick) {
+                wins++;
+                $(".wins").html("Wins: " + wins);
+                console.log("Wins: " + wins);
+                reset();
+
+            }
+
+            else if (newScore > compPick) {
+                losses++;
+                $(".losses").html("Losses: " + losses);
+                console.log("Losses: " + losses);
+                reset();
+
+            }
+
+        });
+
+
+
+    }
+
+    $("img").on("click", function () {
+        var newScore = score += parseInt($(this).attr("value"));
+        console.log("New Score: " + newScore);
+        $(".scoreDisplay").html(newScore);
+
+        if (newScore === compPick) {
+            wins++;
+            $(".wins").html("Wins: " + wins);
+            console.log("Wins: " + wins);
+            reset();
+        }
+
+        else if (newScore > compPick) {
+            losses++;
+            $(".losses").html("Losses: " + losses);
+            console.log("Losses: " + losses);
+            reset();
+        }
+
+    });
+
+}); 
